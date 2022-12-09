d1a4198d7e716d8c2277f54320c589d165763316
abhiraj2912 <abhirajaravind408@gmail.com>
2022-12-09 15:43:56 +0530

    registerapi

--- src/app/user-register/user-register.component.ts
+++ src/app/user-register/user-register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-user-register',
@@ -7,6 +8,7 @@ import { Component } from '@angular/core';
 })
 export class UserRegisterComponent {
 
+  constructor(private api:ApiService){}
 
   name=""
   address=""
@@ -15,11 +17,34 @@ export class UserRegisterComponent {
   email=""
   username=""
   password=""
+  cpassword=""
 
   
   readValues=()=>{
-    let data:any={"name" : this.name, "address" : this.address, "pincode" : this.pincode, "email" : this.email, "phone" : this.phone, "username" : this.username, "password" : this.password}
-    console.log(data)
+    if (this.password==this.cpassword) {
+      let data:any={"name" : this.name, "address" : this.address, "pincode" : this.pincode, "email" : this.email, "phone" : this.phone, "username" : this.username, "password" : this.password}
+      console.log(data)
+      this.api.registerUser(data).subscribe(
+        (response:any)=>{
+          if (response.status=="success") {
+            alert("Registration Successfull")
+            this.name=""
+            this.address=""
+            this.pincode=""
+            this.phone=""
+            this.email=""
+            this.username=""
+            this.password=""
+          } else {
+            alert("Something went wrong")
+          }
+        }
+      )
+      
+    } else {
+      alert("Password not matching")
+    }
+
   }
 
 }
